f8b431541698a0edc31c986c6f834d0133d018ce
prepa2 <40188714+prepa2@users.noreply.github.com>
2018-06-16 23:06:20 -0500

    Create calificaciones.cpp

---
+++ Secuenciales/calificaciones.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float cal1=0,cal2=0,cal3=0;
+
+    cout << "Calificacion matematicas: ";
+    cin >> cal1;
+
+    cout << "Calificacion espa\164ol: ";
+    cin >> cal2;
+
+    cout << "Calificacion matematicas: ";
+    cin >> cal3;
+
+    cout << "El promedio es: " <<(cal1+cal2+cal3)/3;
+
+    return 0;
+
+}
